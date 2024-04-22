0fc6cb495994fac4dfd92b684421309eff41a0ab
Kjthaoo <kthao43726@gmail.com>
2024-04-22 18:56:37 -0400

    added COMMENTS AND THE POST stuff

--- server.js
+++ server.js
@@ -4,20 +4,19 @@ const MongoClient = mongodb.MongoClient;
 const app = express();
 const port = 3000;
 
-// MongoDB connection URL
+// my MongoDB string connection URL or URI and pw
 const mongoURI = 'mongodb+srv://kthao43726:5RUd67PJQX7fPhWl@cluster0.ejq1kfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-// Connect to MongoDB
+// connect to MongoDB
 MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to MongoDB');
-        const db = client.db('your-database-name');
+        const db = client.db('commentsection');
         const commentsCollection = db.collection('comments');
 
-        // Middleware to parse JSON request body
         app.use(express.json());
 
-        // Route to handle POST requests for saving comments
+        // POST - and saving the comments/data
         app.post('/comments', (req, res) => {
             const newComment = req.body;
             commentsCollection.insertOne(newComment)
@@ -34,7 +33,7 @@ MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true 
         console.error('Error connecting to MongoDB:', error);
     });
 
-// Start the Express server
+// then start the express part
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
